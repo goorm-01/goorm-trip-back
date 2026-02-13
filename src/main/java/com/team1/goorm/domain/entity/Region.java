17024e483de19b696c6a7deac127d66311f0b8a3
양진혁 <jin99545@gmail.com>
2026-02-13 17:54:20 +0900

    [FIX] 지역코드 수정(법정동 시도 코드로 교체)

--- src/main/java/com/team1/goorm/domain/entity/Region.java
+++ src/main/java/com/team1/goorm/domain/entity/Region.java
@@ -8,23 +8,23 @@ import java.util.Arrays;
 @Getter
 @RequiredArgsConstructor
 public enum Region {
-    SEOUL("1", "서울"),
-    INCHEON("2", "인천"),
-    DAEJEON("3", "대전"),
-    DAEGU("4", "대구"),
-    GWANGJU("5", "광주"),
-    BUSAN("6", "부산"),
-    ULSAN("7", "울산"),
-    SEJONG("8", "세종"),
-    GYEONGGI("31", "경기"),
-    GANGWON("32", "강원"),
-    CHUNGBUK("33", "충북"),
-    CHUNGNAM("34", "충남"),
-    JEONBUK("35", "전북"),
-    JEONNAM("36", "전남"),
-    GYEONGBUK("37", "경북"),
-    GYEONGNAM("38", "경남"),
-    JEJU("39", "제주");
+    SEOUL("11", "서울"),
+    BUSAN("26", "부산"),
+    DAEGU("27", "대구"),
+    INCHEON("28", "인천"),
+    GWANGJU("29", "광주"),
+    DAEJEON("30", "대전"),
+    ULSAN("31", "울산"),
+    GYEONGGI("41", "경기"),
+    CHUNGBUK("43", "충북"),
+    CHUNGNAM("44", "충남"),
+    JEONNAM("46", "전남"),
+    GYEONGBUK("47", "경북"),
+    GYEONGNAM("48", "경남"),
+    JEJU("50", "제주"),
+    GANGWON("51", "강원"),
+    JEONBUK("52", "전북"),
+    SEJONG("17", "세종");
 
     private final String code;
     private final String description;
