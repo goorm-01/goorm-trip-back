cfe3bdb313107a79613e1e59adac0ba69638b8b5
양진혁 <jin99545@gmail.com>
2026-02-16 01:33:45 +0900

    [FEAT] User Entity 이름 속성 추가

--- src/main/java/com/team1/goorm/domain/entity/User.java
+++ src/main/java/com/team1/goorm/domain/entity/User.java
@@ -16,4 +16,7 @@ public class User {
 
     @Column(name = "email", nullable = false, unique = true)
     private String email;
+
+    @Column(name = "name", nullable = false)
+    private String name;
 }
