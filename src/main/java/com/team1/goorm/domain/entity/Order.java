ee9d0374ed829dcc51c1b4b60fbccae6490f330d
양진혁 <jin99545@gmail.com>
2026-02-16 00:59:26 +0900

    [FEAT] User, Order 엔티티 컬럼명 및 테이블명 수정, 생성자 추가

--- src/main/java/com/team1/goorm/domain/entity/Order.java
+++ src/main/java/com/team1/goorm/domain/entity/Order.java
@@ -1,17 +1,26 @@
 package com.team1.goorm.domain.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "orders")
 public class Order {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(referencedColumnName = "id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
     private User user;
 
     @Column(name = "total_amount")
--- src/main/java/com/team1/goorm/domain/entity/User.java
+++ src/main/java/com/team1/goorm/domain/entity/User.java
@@ -1,13 +1,19 @@
 package com.team1.goorm.domain.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
 
-    @Column(name = "email")
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 }
