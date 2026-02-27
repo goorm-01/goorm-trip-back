92999d4940b80537db3e3ced6a7e453ab20fdf22
양진혁 <jin99545@gmail.com>
2026-02-27 14:44:07 +0900

    [FEAT] ProductController Swagger 설정

--- src/main/java/com/team1/goorm/controller/ProductController.java
+++ src/main/java/com/team1/goorm/controller/ProductController.java
@@ -5,6 +5,8 @@ import com.team1.goorm.domain.dto.ProductDetailDto;
 import com.team1.goorm.domain.dto.ProductDto;
 import com.team1.goorm.service.ProductService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +21,11 @@ public class ProductController {
 
     // 전체 상품 목록 조회
     @GetMapping
+    @Operation(summary = "전체 상품 목록 조회")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "상품 없음")
+    })
     public ResponseEntity<ApiResponse<List<ProductDto>>> getAllProducts() {
         return ResponseEntity.ok(
                 ApiResponse.success("SUCCESS", "전체 상품 정보를 성공적으로 가져왔습니다.", productService.getAllProducts())
@@ -27,6 +34,11 @@ public class ProductController {
 
     // productId를 통한 상세 정보 조회
     @GetMapping("/{productId}")
+    @Operation(summary = "장바구니 목록 조회")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "상품 없음")
+    })
     public ResponseEntity<ApiResponse<ProductDetailDto>> getProductDetail(@PathVariable Long productId) {
         return ResponseEntity.ok(
                 ApiResponse.success("SUCCESS", "상품 상세 조회에 성공했습니다", productService.getProductDetail(productId))
@@ -35,6 +47,11 @@ public class ProductController {
 
     // 카테고리별 조회
     @GetMapping("category/{category}")
+    @Operation(summary = "장바구니 목록 조회")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "상품 없음")
+    })
     public ResponseEntity<ApiResponse<List<ProductDto>>> getProductsByCategory(@PathVariable String category) {
         return ResponseEntity.ok(
                 ApiResponse.success("SUCCESS", "카테고리별 상품 조회에 성공했습니다.", productService.getProductsByCategory(category))
@@ -43,6 +60,11 @@ public class ProductController {
 
     // 지역별 조회
     @GetMapping("/region/{region}")
+    @Operation(summary = "장바구니 목록 조회")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "상품 없음")
+    })
     public ResponseEntity<ApiResponse<List<ProductDto>>> getProductsByRegion(@PathVariable String region) {
         return ResponseEntity.ok(
                 ApiResponse.success("SUCESS", "지역별 상품 조회에 성공했습니다.", productService.getProductsByRegion(region))
